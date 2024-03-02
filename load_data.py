c0248f6391e4e25552fcfbcabc9465b0d702a9cf
Michaela <michaela.philip@emory.edu>
2024-03-02 17:24:36 -0500

    create new variables

--- load_data.py
+++ load_data.py
@@ -5,5 +5,12 @@ from helpers.read import read_data
 main = read_data('data/input/Data_main.dta')
 subsidy = read_data('data/input/Data_subsidyinfo.dta')
 
-print(main.head())
-print(subsidy.head())
+main.to_csv('data/input/main.csv', index=False)
+subsidy.to_csv('data/input/subsidy.csv', index=False)
+
+main['cohort_year'] = main.groupby('uniqueID')['year'].transform('min')
+main['plan_age'] = main['year'] - main['cohort_year']
+main['firm_cohort'] = main.groupby('orgParentCode')['year'].transform('min')
+main['firm_cohort_state'] = main.groupby(['orgParentCode', 'state'])['year'].transform('min')
+
+main.to_csv('data/output/main_plus.csv', index=False)
