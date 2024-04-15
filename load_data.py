2844e43636ab1979efdd63a38ecf349e5b2bf06f
Michaela <michaela.philip@emory.edu>
2024-04-15 10:17:06 -0400

    redefine cohort variables

--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from helpers.read import read_data
 
@@ -8,9 +9,12 @@ subsidy = read_data('data/input/Data_subsidyinfo.dta')
 main.to_csv('data/input/main.csv', index=False)
 subsidy.to_csv('data/input/subsidy.csv', index=False)
 
-main['cohort_year'] = main.groupby('uniqueID')['year'].transform('min')
+main['cohort_year'] = main.groupby('uniqueID')['year'].transform('min') #the year the plan was first offered
 main['plan_age'] = main['year'] - main['cohort_year']
-main['firm_cohort'] = main.groupby('orgParentCode')['year'].transform('min')
-main['firm_cohort_state'] = main.groupby(['orgParentCode', 'state'])['year'].transform('min')
+main['firm_cohort'] = main.groupby('orgParentCode')['year'].transform('min') #the year the firm first offers a plan
+main['firm_cohort_state'] = main.groupby(['orgParentCode', 'state'])['year'].transform('min') #the year that the firm first offers a plan in the state
+main['e_ben'] = np.where(main['benefit'] == 'E', 1, 0)
+main['firm_exists'] = np.where((main['firm_cohort'] < main['year']), 1, 0) #if the firm existed before the year observed
+main['firm_exists_state'] = np.where((main['firm_cohort_state'] < main['year']), 1, 0) #if the firm existed in the state before the year observed
 
 main.to_csv('data/output/main_plus.csv', index=False)
