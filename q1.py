f5204ad6f63321b0e61cf91ef6baae11308dd5d5
Michaela <michaela.philip@emory.edu>
2024-04-15 10:19:42 -0400

    complete q1

--- q1.py
+++ q1.py
@@ -6,14 +6,10 @@ from load_data import main as main
 from load_data import subsidy as subsidy
 
 from helpers.descriptive_statistics import get_stats
-from helpers.tables import create_table
 
 descriptive_stats = get_stats(main)
-descriptive_stats.sort_values(by='year', inplace=True)
-table_1 = create_table(descriptive_stats)
+
+table_1 = descriptive_stats.T
 
 index = ['Mean monthly premium', '  ', 'Mean deductible', '  ', 'Fraction enhanced benefit', 'Fraction of US firms', 'Fraction of state firms', 'Number of unique firms', 'Number of plans']
-
 table_1.index = index
-
-print(table_1)
