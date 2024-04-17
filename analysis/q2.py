67e204fda3ecf4fc1f56298507294f1df765da65
Michaela <michaela.philip@emory.edu>
2024-04-17 08:48:20 -0400

    run model on substet dataframe

--- analysis/q2.py
+++ analysis/q2.py
@@ -16,21 +16,21 @@ results = pd.DataFrame()
 
 #local averages with bin size $0.50, bandwidth of $10, year 2006, no enhanced benefits
 condition1 = ((full_data['rd_window_1'] == 1) & (full_data['year'] == 2006) & (full_data['e_ben']==0))
-bin_scatter = smf.ols(formula = 'ln_share ~ C(bin)', data = full_data[condition1]).fit()
 scatter_data = full_data[condition1]
+bin_scatter = smf.ols(formula = 'ln_share ~ C(bin)', data = scatter_data).fit()
 scatter_data['bin_scatter'] = bin_scatter.predict(scatter_data)
 
 #local linear predition with bandwidth of $4, year 2006, no enhanced benefits
 condition2 = ((full_data['rd_window_2'] == 1) & (full_data['year'] == 2006) & (full_data['e_ben']==0))
-local_lin = smf.ols(formula = 'ln_share ~ below_bench_1 + lis_prem_pos + lis_prem_neg', data = full_data[condition2]).fit(cov_type='cluster', cov_kwds={'groups': full_data['uniqueID'][condition2]})
 lin_data = full_data[condition2]
+local_lin = smf.ols(formula = 'ln_share ~ below_bench_1 + lis_prem_pos + lis_prem_neg', data = lin_data).fit(cov_type='cluster', cov_kwds={'groups': lin_data['uniqueID']})
 lin_data['local_lin'] = local_lin.predict(lin_data)
 lin_data_sort = lin_data.sort_values(by='lis_premium')
 
 #quartic polynomial prediction with bandwith of $10, year 2006, no enhanced benefits
 condition3 = ((full_data['rd_window_1'] == 1) & (full_data['year'] == 2006) & (full_data['e_ben']==0))
-quart_poly = smf.ols(formula = 'ln_share ~ below_bench_1 + lis_prem_pos + lis_prem_neg + lis_prem_pos_sq + lis_prem_neg_sq + lis_prem_pos_cu + lis_prem_neg_cu + lis_prem_pos_qu + lis_prem_neg_qu', data = full_data[condition3]).fit(cov_type='cluster', cov_kwds={'groups': full_data['uniqueID'][condition3]})
 poly_data = full_data[condition3]
+quart_poly = smf.ols(formula = 'ln_share ~ below_bench_1 + lis_prem_pos + lis_prem_neg + lis_prem_pos_sq + lis_prem_neg_sq + lis_prem_pos_cu + lis_prem_neg_cu + lis_prem_pos_qu + lis_prem_neg_qu', data = poly_data).fit(cov_type='cluster', cov_kwds={'groups': poly_data['uniqueID']})
 poly_data['quart_poly'] = quart_poly.predict(poly_data)
 poly_data_sort = poly_data.sort_values(by='lis_premium')
 
