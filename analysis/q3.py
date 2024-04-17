b585ea405549ab9ae17b8d1a22948b82b36446ad
Michaela <michaela.philip@emory.edu>
2024-04-17 11:32:00 -0400

    adjust comments, add graph titles

--- analysis/q3.py
+++ analysis/q3.py
@@ -15,7 +15,7 @@ from helpers.bins import get_bins
 full_data = get_bins(full_data, 2006, 10, 10)
 results = pd.DataFrame()
 
-#local averages with bin size $0.50, bandwidth of $10, year 2006, no enhanced benefits
+#local averages with bin size $1, bandwidth of $10, year 2006, no enhanced benefits
 condition1 = ((full_data['rd_window_1'] == 1) & (full_data['year'] == 2006) & (full_data['e_ben']==0))
 scatter_data = full_data[condition1]
 bin_scatter = smf.ols(formula = 'ln_share ~ C(bin)', data = scatter_data).fit()
@@ -37,12 +37,13 @@ poly_data_sort = poly_data.sort_values(by='lis_premium')
 
 #creating the graph 
 plt.close()
-ytitle = "Log Enrollment Share (10 Bins), 2006"
+ytitle = "Log Enrollment Share, 2006"
 plt.scatter(scatter_data['bin'], scatter_data['bin_scatter'], label='Scatter')
 plt.plot(lin_data_sort['lis_premium'], lin_data_sort['local_lin'], linestyle='dashed', color='gray', label='Local Linear')
 plt.plot(poly_data_sort['lis_premium'], poly_data_sort['quart_poly'], linestyle='solid', color='black', label='Quartic Polynomial')
 plt.xlabel("Monthly Premium - LIS Subsidy, 2006")
 plt.ylabel(ytitle)
+plt.title("Effect of Benchmark Status on Enrollment (10 Bins)")
 plt.legend()
 plt.savefig('data/output/Figure3a.png')
 plt.show()
@@ -52,7 +53,7 @@ plt.show()
 full_data = get_bins(full_data, 2006, 30, 10)
 results = pd.DataFrame()
 
-#local averages with bin size $0.50, bandwidth of $10, year 2006, no enhanced benefits
+#local averages with bin size $0.33, bandwidth of $10, year 2006, no enhanced benefits
 condition1 = ((full_data['rd_window_1'] == 1) & (full_data['year'] == 2006) & (full_data['e_ben']==0))
 scatter_data = full_data[condition1]
 bin_scatter = smf.ols(formula = 'ln_share ~ C(bin)', data = scatter_data).fit()
@@ -74,12 +75,13 @@ poly_data_sort = poly_data.sort_values(by='lis_premium')
 
 #creating the graph 
 plt.close()
-ytitle = "Log Enrollment Share (30 Bins), 2006"
+ytitle = "Log Enrollment Share, 2006"
 plt.scatter(scatter_data['bin'], scatter_data['bin_scatter'], label='Scatter')
 plt.plot(lin_data_sort['lis_premium'], lin_data_sort['local_lin'], linestyle='dashed', color='gray', label='Local Linear')
 plt.plot(poly_data_sort['lis_premium'], poly_data_sort['quart_poly'], linestyle='solid', color='black', label='Quartic Polynomial')
 plt.xlabel("Monthly Premium - LIS Subsidy, 2006")
 plt.ylabel(ytitle)
+plt.title("Effect of Benchmark Status on Enrollment (30 Bins)")
 plt.legend()
 plt.savefig('data/output/Figure3b.png')
 plt.show()
