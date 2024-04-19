f99f5a8f58eb0d29a261fe76c462dfd83d022a17
Michaela <michaela.philip@emory.edu>
2024-04-19 15:40:49 -0400

    remove ians code

--- analysis/q8.py
+++ analysis/q8.py
@@ -10,20 +10,6 @@ sys.path.append(os.path.join(script_dir, '..'))
 
 from data_code.load_clean_data import full_data as full_data
 
-# data.2007 <- final.data %>%
-#   filter(year==2007) %>%
-#   select(orgParentCode, planName, state, contractId, uniqueID,
-#          premium_2007=premium)
-
-# reg.data <- data.2006 %>%
-#   left_join(data.2007, by=c("orgParentCode","planName","state",
-#                             "contractId","uniqueID")) %>%
-#   mutate(premium_diff=premium_2007-premium_2006)
-
-# mean.share <- round(as.numeric(reg.data %>% summarize(mean_share=mean(share, na.rm=TRUE))), 3)
-# inertia <- feols(premium_diff~1 | ln_share~LISPremium_2006, data=reg.data)
-
-
 #value of premium (without subsidy) in 2006
 data_2006 = full_data[full_data['year'] == 2006]
 premium_2006 = data_2006[['uniqueID', 'premium']].drop_duplicates()
