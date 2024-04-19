83dc0cb51e27a39dffc07f94a75e8e5fb83c64e4
Michaela <michaela.philip@emory.edu>
2024-04-19 15:44:14 -0400

    comment out print statement

--- analysis/q5.py
+++ analysis/q5.py
@@ -15,8 +15,8 @@ rd_test = rddensity(full_data['lis_premium'])
 rd_test_pval = rd_test.p
 rd_test_diff = rd_test.hat
 
-print("The p-value for the manipulation test is " + str(rd_test_pval))
-print("The difference in estimated density at the cutoff is " + str((rd_test_diff.right - rd_test_diff.left).round(4)))
+# print("The p-value for the manipulation test is " + str(rd_test_pval))
+# print("The difference in estimated density at the cutoff is " + str((rd_test_diff.right - rd_test_diff.left).round(4)))
 
 density_plot = rdplotdensity(rd_test, full_data['lis_premium'], xlabel = 'Monthly Premium - LIS Subsidy', title = 'Manipulation Test')
 # print(density_plot)
